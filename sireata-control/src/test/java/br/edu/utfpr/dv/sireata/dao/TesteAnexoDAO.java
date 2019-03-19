a762a7e914077fdd493675bbb0c41ccad9119f0b
Gabriel Costa Silva <gabriel@Gabriels-iMac.local>
2019-03-19 18:17:54 -0300

    - Created all tests for AnexoDAO;

--- sireata-control/src/test/java/br/edu/utfpr/dv/sireata/dao/TesteAnexoDAO.java
+++ sireata-control/src/test/java/br/edu/utfpr/dv/sireata/dao/TesteAnexoDAO.java
@@ -1,11 +1,11 @@
 package br.edu.utfpr.dv.sireata.dao;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 
 import java.sql.SQLException;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.edu.utfpr.dv.sireata.model.Anexo;
@@ -13,34 +13,29 @@ import br.edu.utfpr.dv.sireata.model.Ata;
 
 public class TesteAnexoDAO {
 
-    private static Anexo anexo;
-    private static AnexoDAO dao;
-
-    @BeforeClass
-    public static void setup() {
-        anexo = new Anexo();
+    @Test
+    public void testaSalvar() throws SQLException {
+        Anexo anexo = new Anexo();
         anexo.setDescricao("descricao");
         anexo.setArquivo(new byte[] {1,2,3});
         anexo.setAta(new Ata());
         anexo.setOrdem(10);
 
-        dao = new AnexoDAO();
-    }
+        AnexoDAO dao = new AnexoDAO();
 
-    @Test
-    public void testaSalvar() throws SQLException {
         dao.salvar(anexo);
     }
 
     @Test
-    public void testaBuscarPorId() throws SQLException {
-        int id = dao.salvar(anexo);
-
-        assertEquals(anexo.getDescricao(), dao.buscarPorId(id).getDescricao());
-    }
-
-    @Test
     public void testaExcluir() throws SQLException {
+        Anexo anexo = new Anexo();
+        anexo.setDescricao("descricao");
+        anexo.setArquivo(new byte[] {1,2,3});
+        anexo.setAta(new Ata());
+        anexo.setOrdem(10);
+
+        AnexoDAO dao = new AnexoDAO();
+
         int id = dao.salvar(anexo);
 
         assertEquals(anexo.getDescricao(), dao.buscarPorId(id).getDescricao());
@@ -48,6 +43,37 @@ public class TesteAnexoDAO {
         dao.excluir(id);
 
         assertNull(dao.buscarPorId(id));
+    }
+
+    @Test
+    public void testaBuscarPorId() throws SQLException {
+        Anexo anexo = new Anexo();
+        anexo.setDescricao("descricao");
+        anexo.setArquivo(new byte[] {1,2,3});
+        anexo.setAta(new Ata());
+        anexo.setOrdem(10);
+
+        AnexoDAO dao = new AnexoDAO();
+
+        int id = dao.salvar(anexo);
+
+        assertEquals(anexo.getDescricao(), dao.buscarPorId(id).getDescricao());
+    }
+
+
+    @Test
+    public void testaListarPorAta() throws SQLException {
+        Anexo anexo = new Anexo();
+        anexo.setDescricao("descricao");
+        anexo.setArquivo(new byte[] {1,2,3});
+        anexo.setAta(new Ata());
+        anexo.setOrdem(10);
+
+        AnexoDAO dao = new AnexoDAO();
+
+        int id = dao.salvar(anexo);
+
+        assertNotEquals(0, dao.listarPorAta(0).size());
     }
 
     
