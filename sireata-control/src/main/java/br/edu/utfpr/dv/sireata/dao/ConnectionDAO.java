cf52bf96c9a4dfce785dff462733cacd8e0539d9
Gabriel Costa Silva <gabriel@Gabriels-iMac.local>
2019-03-19 17:35:43 -0300

    - Fixed issues w/ ConnectionDAO; - Created tests for AnexoDAO;

--- sireata-control/src/main/java/br/edu/utfpr/dv/sireata/dao/ConnectionDAO.java
+++ sireata-control/src/main/java/br/edu/utfpr/dv/sireata/dao/ConnectionDAO.java
@@ -1,84 +1,82 @@
 package br.edu.utfpr.dv.sireata.dao;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-// Updated to use Derby (in memory) DB to simplify the 
+// Updated to use Derby (local) DB to simplify the 
 // experimental execution. Note that this change does not 
 // impact experimental tasks, but it just simplify the 
 // experimental environment setup.
 
 public class ConnectionDAO {
 
-	private static ConnectionDAO instance = null;
-	private Connection conn;
-	
-	private ConnectionDAO() { }
-	
-	public static synchronized ConnectionDAO getInstance() throws SQLException{
-		if(instance == null){
-			instance = new ConnectionDAO();
-			instance.createDataSource();
-		}
-		
-		return instance;
-	}
-	
-	public Connection getConnection() throws SQLException{
-        return conn;
-	}
-	
-	private void createDataSource() {
+	private String connectionUrl = "jdbc:derby:database;create=true";
+	private Connection conn = null;
 
-		// Create tables in the database
-		try {
-			conn = DriverManager.getConnection("jdbc:derby:database;create=true"); 
+	private static ConnectionDAO connectionFactory = null;
+
+	private ConnectionDAO() { }
+
+	public Connection getConnection() throws SQLException {
+		
+		conn = DriverManager.getConnection(connectionUrl);
+
+		DatabaseMetaData dbmd = conn.getMetaData();
+		ResultSet rs = dbmd.getTables(null, null, "%", null);
+
+		boolean tableExists = false;
+		while (rs.next())
+			if (rs.getString(3).equalsIgnoreCase("pautas"))
+				tableExists = true;
+			  
+
+		if(!tableExists)
+			createTables(); 
+
+		return conn;
+	}
+
+	public static ConnectionDAO getInstance() {
+		if (connectionFactory == null) {
+			connectionFactory = new ConnectionDAO();
+		}
+
+		return connectionFactory;
+	}
+
+	private void createTables() {
+
+		System.out.println("Creating tables ...");
+
+		try {			
 			conn.createStatement().executeUpdate(
 					"CREATE TABLE pautas (" +
-						"idpauta int NOT NULL," +
+						"idpauta int NOT NULL GENERATED ALWAYS AS IDENTITY CONSTRAINT IDPAUTA_PK PRIMARY KEY," +
 						"idata int NOT NULL," +
 						"ordem int NOT NULL," + 
 						"titulo VARCHAR(255) NOT NULL," + 
 						"descricao VARCHAR(255) NOT NULL)");
-					// 	," +
-					// 	"PRIMARY KEY (idpauta)," +
-					// 	"KEY fk_pauta_ata_idx (idata)," + 
-					// 	"CONSTRAINT fk_pauta_ata FOREIGN KEY (idata) REFERENCES atas (idata)"+ 
-					// 	"ON DELETE NO ACTION ON UPDATE NO ACTION" + 
-					// ");");
 
 			conn.createStatement().executeUpdate(
 					"CREATE TABLE ataparticipantes (" +
-						"idataparticipante int NOT NULL," +
+						"idataparticipante int NOT NULL GENERATED ALWAYS AS IDENTITY CONSTRAINT IDATAPARTICIPANTE_PK PRIMARY KEY," +
 						"idata int NOT NULL," + 
 						"idusuario int NOT NULL," +
 						"designacao VARCHAR(50) NOT NULL," +
 						"presente int NOT NULL," +
 						"membro int NOT NULL," + 
 						"motivo VARCHAR(255) NOT NULL)");
-					// 	,"+ 
-					// 	"PRIMARY KEY (idataparticipante)," +
-					// 	"KEY fk_ataparticipantes_ata_idx (idata)," +
-					// 	"CONSTRAINT fk_ataparticipantes_ata FOREIGN KEY (idata) REFERENCES atas (idata) ON DELETE NO ACTION" + 
-					// 	"ON UPDATE NO ACTION," +
-					// 	"KEY fk_ataparticipantes_usuario_idx (idusuario)," +
-					// 	"CONSTRAINT fk_ataparticipantes_usuario FOREIGN KEY (idusuario) REFERENCES usuarios (idusuario) ON" + 
-					// 	"DELETE NO ACTION ON UPDATE NO ACTION" +
-					// ");");
 
 			conn.createStatement().executeUpdate(
 					"CREATE TABLE anexos (" +
-						"idanexo INT NOT NULL," +
+						"idanexo INT NOT NULL GENERATED ALWAYS AS IDENTITY CONSTRAINT IDANEXO_PK PRIMARY KEY," +
 						"idata INT NOT NULL," +
 						"descricao VARCHAR(50) NOT NULL," +
 						"ordem INT NOT NULL," +
 						"arquivo blob NOT NULL)");
-					// 	," +
-					// 	"PRIMARY KEY (idanexo)," +
-					// 	"CONSTRAINT fk_anexos_ata FOREIGN KEY (idata) REFERENCES atas (idata) ON DELETE NO ACTION ON UPDATE"+ "NO ACTION," +
-					// 	"KEY fk_anexos_ata_idx (idata)" +
-					// ");");
 
 		} catch (SQLException ex) {
 			ex.printStackTrace();
--- sireata-control/src/test/java/br/edu/utfpr/dv/sireata/dao/TesteAnexoDAO.java
+++ sireata-control/src/test/java/br/edu/utfpr/dv/sireata/dao/TesteAnexoDAO.java
@@ -1,7 +1,11 @@
 package br.edu.utfpr.dv.sireata.dao;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import java.sql.SQLException;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.edu.utfpr.dv.sireata.model.Anexo;
@@ -9,19 +13,41 @@ import br.edu.utfpr.dv.sireata.model.Ata;
 
 public class TesteAnexoDAO {
 
-    @Test
-    public void testaSalvar() throws SQLException {
-        Anexo anexo = new Anexo();
-        anexo.setIdAnexo(10);
+    private static Anexo anexo;
+    private static AnexoDAO dao;
+
+    @BeforeClass
+    public static void setup() {
+        anexo = new Anexo();
         anexo.setDescricao("descricao");
         anexo.setArquivo(new byte[] {1,2,3});
         anexo.setAta(new Ata());
         anexo.setOrdem(10);
 
-        AnexoDAO dao = new AnexoDAO();
+        dao = new AnexoDAO();
+    }
 
+    @Test
+    public void testaSalvar() throws SQLException {
         dao.salvar(anexo);
-        
+    }
+
+    @Test
+    public void testaBuscarPorId() throws SQLException {
+        int id = dao.salvar(anexo);
+
+        assertEquals(anexo.getDescricao(), dao.buscarPorId(id).getDescricao());
+    }
+
+    @Test
+    public void testaExcluir() throws SQLException {
+        int id = dao.salvar(anexo);
+
+        assertEquals(anexo.getDescricao(), dao.buscarPorId(id).getDescricao());
+
+        dao.excluir(id);
+
+        assertNull(dao.buscarPorId(id));
     }
 
     
